8cd046201befcfbb1fcfa3a3b33664f0506df504
Morten Rasmussen <morten.rasmussen@arm.com>
2016-04-29 20:32:40 +0100

    sched/fair: Correct unit of load_above_capacity
    In calculate_imbalance() load_above_capacity currently has the unit
    [capacity] while it is used as being [load/capacity]. Not only is it
    wrong it also makes it unlikely that load_above_capacity is ever used
    as the subsequent code picks the smaller of load_above_capacity and
    the avg_load
    
    This patch ensures that load_above_capacity has the right unit
    [load/capacity].
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    [ Changed changelog to note it was in capacity unit; +rebase. ]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/1461958364-675-4-git-send-email-dietmar.eggemann@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8932,9 +8932,11 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 	    local->group_type   == group_overloaded) {
 		load_above_capacity = busiest->sum_nr_running *
 					SCHED_LOAD_SCALE;
-		if (load_above_capacity > busiest->group_capacity)
+		if (load_above_capacity > busiest->group_capacity) {
 			load_above_capacity -= busiest->group_capacity;
-		else
+			load_above_capacity *= NICE_0_LOAD;
+			load_above_capacity /= busiest->group_capacity;
+		} else
 			load_above_capacity = ~0UL;
 	}
 
