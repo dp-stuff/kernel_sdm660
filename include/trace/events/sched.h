be2afcae9f7af14daa82d3e73be642934f1c8da7
Quentin Perret <quentin.perret@arm.com>
2017-08-30 16:54:44 +0100

    sched: compute task utilisation with WALT consistently
    Using WALT, the utilisation of a task is computed with a resolution
    scaling factor that has been used inconsistently in the code with either
    hardcoded values or macros (NICE_0_LOAD_SHIFT in this case). Changes in
    these macros (as the 32 to 64 bits resolution shift of 2159197d66770)
    happened to break the utilisation calculation wherever they have been
    used whilst results remained correct in other places. This commit fixes
    this issue by using SCHED_CAPACITY_SCALE as resolution scaling factor
    consistently.
    
    Change-Id: Ic5418f8a5dfc455a22bafbebb4142b4665b61c6f
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -1183,7 +1183,7 @@ TRACE_EVENT(walt_update_history,
 		__entry->samples        = samples;
 		__entry->evt            = evt;
 		__entry->demand         = p->ravg.demand;
-		__entry->walt_avg	= (__entry->demand << 10);
+		__entry->walt_avg	= (__entry->demand << SCHED_CAPACITY_SHIFT);
 		do_div(__entry->walt_avg, walt_ravg_window);
 		__entry->pelt_avg	= p->se.avg.util_avg;
 		memcpy(__entry->hist, p->ravg.sum_history,
